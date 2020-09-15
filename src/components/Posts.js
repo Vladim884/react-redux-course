1ca0206af8fe05f851a7b4002a6c77ec9af79097
vladim <vladim.volovenko@gmail.com.com>
2020-09-15 20:16:31 +0300

    createStore, rootReducer, postsReducer, connect, mapStateToProps in posts.js

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { connect } from "react-redux";
 import Post from "./Post";
 
 
-export default ({posts}) => {
+const Posts = ({syncPosts}) => {
     if(!posts.length){
         return <p className="text-center">постов пока нет</p>
     }
   return posts.map(post => <Post post={post} key={post} /> );
 };
+
+const mapStateToProps = (state) => {
+    console.log(state);
+    return {
+        syncPosts: state.posts.posts
+    } 
+}
+
+export default connect(mapStateToProps, null)(Posts);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { compose, createStore } from 'redux';
 import App from './App';
+import { rootReducer } from './redux/rootReducer';
+import {Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore(
+  rootReducer, compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  );
 
 render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
