c8d8dcf3fc72421516ef443f42b327f50dc9be14
vladim <vladim.volovenko@gmail.com.com>
2020-09-15 16:48:05 +0300

    finished base structure

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -5,11 +5,19 @@ export default class PostForm extends React.Component {
     super(props);
     this.state = {};
   }
+
+  submitHandler = (e) => {
+    e.preventDefault();
+  };
   render() {
     return (
-      <div>
-        <h1>Post Form</h1>
-      </div>
+      <form onSubmit={this.submitHandler}>
+        <div className="form-group">
+          <label for="title">Заголовок поста</label>
+          <input type="text" className="form-control" id="title" />
+        </div>
+        <button type="submit" class="btn btn-success">Создать</button>
+      </form>
     );
   }
 }
