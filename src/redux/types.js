daa67e63c7d0f4c6e7097a4abfee1b39d345a638
vladim <vladim.volovenko@gmail.com.com>
2020-09-16 14:58:25 +0300

    connected asynchronous posts from the server

--- src/redux/types.js
+++ src/redux/types.js
@@ -1,1 +1,4 @@
-export const CREATE_POST = 'post/CREATE_POST'
+export const CREATE_POST = 'post/CREATE_POST';
+export const FETCH_POSTS = 'post/FETCH_POSTS';
+export const SHOW_LOADER = 'APP/SHOW_LOADER';
+export const HIDE_LOADER = 'APP/HIDE_LOADER';
